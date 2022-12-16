480e76a5fe77c118a75ad17261669e3de4b32dbc
miguellopezpatricio <ml.patri@gmail.com>
2022-12-16 14:30:25 +0100

    creación de componentes

---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html'
+})
+export class PaisInputComponent {
+
+  @Output() onEnter: EventEmitter<string> = new EventEmitter()
+
+  termino: string = ''
+  buscar(){
+
+    this.onEnter.emit(this.termino)
+  }
+
+
+}
--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,22 +10,26 @@ export class PorPaisComponent {
 
  termino: string =''
  hayError: boolean = false
+ paises: Country[] = []
 
  constructor(private paisService:PaisService){
 
  }
 
- buscar(){
+ buscar(termino:string){
 
   this.hayError = false
-
-  console.log(this.termino)
+  this.termino = termino
+  
 
   this.paisService.buscarPais(this.termino)
-  .subscribe((resp) => {
-    console.log(resp)
+  .subscribe((paises) => {
+    console.log(paises)
+    this.paises = paises
+    
   },(err)=>{
     this.hayError = true
+    this.paises = []
     console.log('error')
     console.info(err)
 
