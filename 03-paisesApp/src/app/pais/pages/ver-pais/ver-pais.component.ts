3d3e38f4b5ba0bda2f15bf4b6ca4f5b2d01cfdf4
miguellopezpatricio <ml.patri@gmail.com>
2022-12-17 12:14:51 +0100

    Inicio nuevo proyecto pipesApp

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styleUrls: ['./ver-pais.component.css']
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit{
+
+
+  pais!: Country
+
+  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService){
+
+  }
+
+
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+        )
+      .subscribe(pais => this.pais = pais)
+
+
+    // this.activatedRoute.params
+    // .subscribe(({id})=>{
+    //   console.log(id)
+
+    //   this.paisService.getPaisPorAlpha(id)
+    //   .subscribe(pais=>{
+    //     console.log(pais)
+    //   })
+
+    // })
+
+
+  }
 
 }
--- 03-paisesApp/src/app/pais/services/pais.service.ts
+++ 03-paisesApp/src/app/pais/services/pais.service.ts
@@ -24,4 +24,9 @@ export class PaisService {
     const url = `${this.apiUrl}/capital/${termino}`
     return this.http.get<Country[]>(url)
   }
+
+  getPaisPorAlpha(id:string):Observable<Country>{
+    const url = `${this.apiUrl}/alpha/${id}`
+    return this.http.get<Country>(url)
+  }
 }
