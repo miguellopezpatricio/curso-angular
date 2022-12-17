716701dd49749c856759b9039b429540d386e9c1
miguellopezpatricio <ml.patri@gmail.com>
2022-12-17 12:37:33 +0100

    Actualizado README.md

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre:string = 'miguEL lOpeZ'
+
+  mostrarNombre(){
+    console.log(this.nombre)
+  }
 }
