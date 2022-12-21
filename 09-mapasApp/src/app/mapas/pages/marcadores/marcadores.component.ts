c099ad053fa58c5cbaf78b077ffbd824d6cf1e6c
miguellopezpatricio <ml.patri@gmail.com>
2022-12-21 12:21:31 +0100

    Finalizando 9-mapaApp

--- 09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ 09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,9 +1,168 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+import { map } from 'rxjs';
+
+
+interface MarcadorColor{
+  color: string
+
+  // opcionales
+  marker?: mapboxgl.Marker
+  centro?: [number, number]
+}
+
 
 @Component({
   selector: 'app-marcadores',
-  templateUrl: './marcadores.component.html'
-})
-export class MarcadoresComponent {
+  templateUrl: './marcadores.component.html',
+  styles:[
+      `
+      .mapa-container{ 
+          position: fixed;
+          top: 0px;
+          right: 0px;
+          height: 100vh;
+          width: 100vw;
+    }
 
+    .list-group{
+      position:fixed;
+      top:20px;
+      right:20px;
+      z-index:999;
+    }
+
+    li{
+      cursor:pointer;
+    }
+    `
+    ]
+})
+
+
+export class MarcadoresComponent implements AfterViewInit{
+
+  @ViewChild('mapa') divMapa!: ElementRef
+  mapa!: mapboxgl.Map
+  zoomLevel: number = 10
+  center: [number,number] = [-8.66149407728201, 42.31345545371591]
+
+  // Array de marcadores
+  marcadores: MarcadorColor[]=[]
+
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center:this.center,
+      zoom: this.zoomLevel
+    });
+
+    this.leerLocalStorage()
+
+    // const markerHtml: HTMLElement = document.createElement('div')
+    // markerHtml.innerHTML = 'ESTÁS AQUÍ'
+
+    // const marker = new mapboxgl.Marker({
+    //   element:markerHtml
+    // })
+    // .setLngLat(this.center)
+    // .addTo(this.mapa)
+  }
+
+
+  irMarcador(marker: mapboxgl.Marker){
+    this.mapa.flyTo({
+      center: marker.getLngLat()
+    })
+  }
+
+  agregarMarcador(){
+
+    // genera un color aleatorio
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
+    .setLngLat(this.center)
+    .addTo(this.mapa)
+
+    this.marcadores.push({
+      color, 
+      marker: nuevoMarcador
+    })
+
+    this.guardarMarcadorLocalStorage()
+
+        // listener cuando se deja de arrastrar un marcador en el mapa
+        nuevoMarcador.on('dragend',()=>{
+          this.guardarMarcadorLocalStorage()
+        })
+
+  }
+
+  guardarMarcadorLocalStorage(){
+
+    const lngLatArr: MarcadorColor[] = []
+
+    this.marcadores.forEach(m => {
+
+      const color = m.color
+      const {lng,lat} = m.marker!.getLngLat()
+
+      lngLatArr.push({
+        color: color,
+        centro: [lng, lat]
+      })
+
+    })
+
+    // Almacenando el marcador en localstorage para no perderlo al cambiar de componente
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr))
+  }
+
+  leerLocalStorage(){
+
+    if(!localStorage.getItem('marcadores')){
+      return
+    }
+
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!)
+
+    lngLatArr.forEach(m => {
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true
+      })
+      .setLngLat(m.centro!)
+      .addTo(this.mapa)
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color
+      })
+
+
+      // listener cuando se deja de arrastrar un marcador en el mapa
+      newMarker.on('dragend',()=>{
+        this.guardarMarcadorLocalStorage()
+      })
+
+    })
+
+  }
+
+
+  borrarMarcador(i:number){
+    // eliminamos el marcador en el mapa
+    this.marcadores[i].marker?.remove()
+
+    // eliminamos el marcador del arreglo de marcadores en el localstorage
+    this.marcadores.splice(i,1)
+    this.guardarMarcadorLocalStorage()
+  }
 }
+
--- 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 
@@ -28,14 +28,22 @@ import * as mapboxgl from 'mapbox-gl';
   ]
 })
 
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef
   mapa!: mapboxgl.Map
   zoomLevel: number = 10
   center: [number,number] = [-8.66149407728201, 42.31345545371591]
 
+
   constructor(){}
+
+  // destrucción de instancias
+  ngOnDestroy(): void {
+    this.mapa.off('zoom',()=>{})
+    this.mapa.off('zoomend',()=>{})
+    this.mapa.off('move',()=>{})
+  }
 
 
     ngAfterViewInit(): void {
@@ -58,6 +66,14 @@ export class ZoomRangeComponent implements AfterViewInit {
       }
 
     })
+
+    // conocer el movimiento del mapa
+    this.mapa.on('move', (event)=>{
+      const target = event.target
+      const {lng, lat} = target.getCenter()
+      this.center = [lng, lat]
+    })
+
   }
   
 
